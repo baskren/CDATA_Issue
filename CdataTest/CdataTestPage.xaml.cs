4a27691be6469bc757d95ca6ee0e6570010f9e4c
Ben Askren <ben@buildcalc.com>
2016-05-05 09:44:55 -0400

    Initial Commit

---
+++ CdataTest/CdataTestPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CdataTest
+{
+	public partial class CdataTestPage : ContentPage
+	{
+		public CdataTestPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
